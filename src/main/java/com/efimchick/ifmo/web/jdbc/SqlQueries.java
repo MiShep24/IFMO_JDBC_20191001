2ba7a14f2896dc6ac74bb453f1e590ea681a0d2d
MiShep24 <55436440+MiShep24@users.noreply.github.com>
2019-10-26 02:10:00 +0300

    Add files via upload

--- src/main/java/com/efimchick/ifmo/web/jdbc/SqlQueries.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/SqlQueries.java
@@ -3,46 +3,46 @@ package com.efimchick.ifmo.web.jdbc;
 public class SqlQueries {
     //Select all employees sorted by last name in ascending order
     //language=HSQLDB
-    String select01 = "select * from EMPLOYEE order by lastname ASC";
+    public String select01 = "select * from EMPLOYEE order by lastname ASC";
 
     //Select employees having no more than 5 characters in last name sorted by last name in ascending order
     //language=HSQLDB
-    String select02 = "select * from EMPLOYEE where LENGTH(lastname) <=5 order by lastname ASC";
+    public String select02 = "select * from EMPLOYEE where LENGTH(lastname) <=5 order by lastname ASC";
 
     //Select employees having salary no less than 2000 and no more than 3000
     //language=HSQLDB
-    String select03 = "select * from EMPLOYEE where salary between 2000 and 3000";
+    public String select03 = "select * from EMPLOYEE where salary between 2000 and 3000";
 
     //Select employees having salary no more than 2000 or no less than 3000
     //language=HSQLDB
-    String select04 = "select * from EMPLOYEE where salary <= 2000 or salary >= 3000";
+    public String select04 = "select * from EMPLOYEE where salary <= 2000 or salary >= 3000";
 
     //Select employees assigned to a department and corresponding department name
     //language=HSQLDB
-    String select05 = "select * from EMPLOYEE inner join department on EMPLOYEE.department = department.id";
+    public String select05 = "select * from EMPLOYEE inner join department on EMPLOYEE.department = department.id";
 
     //Select all employees and corresponding department name if there is one.
     //Name column containing name of the department "depname".
     //language=HSQLDB
-    String select06 = "select EMPLOYEE.id, EMPLOYEE.firstname, EMPLOYEE.department, EMPLOYEE.lastname, EMPLOYEE.salary, EMPLOYEE.middlename, department.id, department.name as \"depname\"" +
+    public String select06 = "select EMPLOYEE.id, EMPLOYEE.firstname, EMPLOYEE.department, EMPLOYEE.lastname, EMPLOYEE.salary, EMPLOYEE.middlename, department.id, department.name as \"depname\"" +
             "from EMPLOYEE full join department on EMPLOYEE.department = department.id where EMPLOYEE.middlename is not null";
 
     //Select total salary pf all employees. Name it "total".
     //language=HSQLDB
-    String select07 = "select SUM(salary) AS \"total\" from EMPLOYEE";
+    public String select07 = "select SUM(salary) AS \"total\" from EMPLOYEE";
 
     //Select all departments and amount of employees assigned per department
     //Name column containing name of the department "depname".
     //Name column containing employee amount "staff_size".
     //language=HSQLDB
-    String select08 = "select department.name AS \"depname\", COUNT(EMPLOYEE.id) AS \"staff_size\" "+
+    public String select08 = "select department.name AS \"depname\", COUNT(EMPLOYEE.id) AS \"staff_size\" "+
             "from department inner join EMPLOYEE ON EMPLOYEE.department = department.id "+
             "group by department.name";
 
     //Select all departments and values of total and average salary per department
     //Name column containing name of the department "depname".
     //language=HSQLDB
-    String select09 = "select department.name AS \"depname\", SUM(EMPLOYEE.salary) AS \"total\", AVG(EMPLOYEE.salary) AS \"average\" "+
+    public String select09 = "select department.name AS \"depname\", SUM(EMPLOYEE.salary) AS \"total\", AVG(EMPLOYEE.salary) AS \"average\" "+
             "from department inner join EMPLOYEE on EMPLOYEE.department = department.id "+
             "group by department.name";
 
@@ -50,7 +50,7 @@ public class SqlQueries {
     //Name column containing employee lastname "employee".
     //Name column containing manager lastname "manager".
     //language=HSQLDB
-    String select10 = "select employee1.lastname AS \"employee\", employee2.lastname AS \"manager\" "+
+    public String select10 = "select employee1.lastname AS \"employee\", employee2.lastname AS \"manager\" "+
             "from EMPLOYEE employee1 left join EMPLOYEE employee2 on employee1.manager = employee2.id";
 
 
